a7c8cd27ad0b8901831b254cf7c3476fd0049b20
Björn Nordstrand <bjorn@def-end.se>
2024-03-11 14:05:15 +0100

    Minor update to dns server list

--- main.exs
+++ main.exs
@@ -4,24 +4,26 @@ domain = System.argv()
 
 defmodule GlobalDig do
   @dns_servers %{
-    bahnhof_se: "213.80.98.2",
     cloudflare: "1.1.1.1",
-    comodo_us: "8.26.56.26",
     dyn: "216.146.35.35",
     freedns: "37.235.1.174",
-    foxtel_net: "172.193.67.34",
     google: "8.8.8.8",
     level3: "209.244.0.3",
     open_dns: "208.67.222.222",
     safe_dns: "195.46.39.39",
-    verisign_us: "64.6.64.6",
-    hinet_tw: "168.95.1.1",
     quad9: "9.9.9.9",
-    securolytics_ca: "144.217.51.168",
-    uunet_ch: "195.129.12.122",
-    uunet_de: "192.76.144.66",
-    uunet_uk: "158.43.240.3",
-    uunet_us: "198.6.100.25"
+    bahnhof_se: "213.80.98.2", # Sweden
+    comodo_us: "8.26.56.26", # US
+    dinamic: "181.205.187.218", # Colombia
+    foxtel_net: "172.193.67.34", # Australia
+    singnet: "58.185.92.216", # Singapore
+    verisign_us: "64.6.64.6", # US
+    hinet_tw: "168.95.1.1", # Taiwan
+    securolytics_ca: "144.217.51.168", # Canada
+    uunet_ch: "195.129.12.122", # Switzerland
+    uunet_de: "192.76.144.66", # Germany
+    uunet_uk: "158.43.240.3", # UK
+    uunet_us: "198.6.100.25" # US
   }
 
   def start(pid, domain) do
